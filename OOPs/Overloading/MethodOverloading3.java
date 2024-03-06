85cd12c7736e0f4a9058d6b33e4335eaeb088256
chanchalmishra <35187244+mishrack@users.noreply.github.com>
2024-03-06 07:48:51 +0530

    Update MethodOverloading3.java

--- OOPs/Overloading/MethodOverloading3.java
+++ OOPs/Overloading/MethodOverloading3.java
@@ -5,6 +5,7 @@ public class MethodOverloading3 {
         // 6. In Overloading method resolution is always taken care by reference type only
         Parent1 p = new Child1();
         p.m1();  // at compile time parent class method will be called
+       // p.m2();  // cannot resolve method m2() in Parent1
     }
 }
 class Parent1{
@@ -15,5 +16,8 @@ class Parent1{
 class  Child1 extends Parent1{
     void m1(){
         System.out.println("inside child-1 m1() method");
+    }
+    void m2(){
+        System.out.println("inside child-1 m2() method");
     }
 }
